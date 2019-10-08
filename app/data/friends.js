f99c9eec3c7410f7770c893d88f277276a12cb12
danlgrigg <griggdl@gmail.com>
2019-10-08 01:03:19 -0700

    Added post request to apiroutes.  Added modal jquery to display match.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,225 +1,38 @@
 module.exports = [
   {
-    name: "Ahmed",
+    name: "Jenny M.",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
     scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
   },
   {
-    name: "Jacob Deming",
+    name: "Tobias L.",
     photo:
       "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
     scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   },
   {
-    name: "Jeremiah Scanlon",
+    name: "Ellie Belle",
     photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
     scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
   },
   {
-    name: "Louis T. Delia",
+    name: "Will F.",
     photo:
       "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
     scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
   },
   {
-    name: "Lou Ritter",
+    name: "Amy W.",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
     scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
   },
   {
-    name: "Jordan Biason",
+    name: "Jimbob M.",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
     scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
   },
-  {
-    name: "Shivali",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["5", "2", "2", "2", "2", "2", "3", "2", "4", "2"]
-  },
-  {
-    name: "emily",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["2", "4", "2", "1", "1", "4", "3", "2", "4", "1"]
-  },
-  {
-    name: "emily",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["2", "4", "2", "1", "1", "4", "3", "2", "4", "1"]
-  },
-  {
-    name: "emily",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["2", "4", "2", "1", "1", "4", "3", "2", "4", "1"]
-  },
-  {
-    name: "emily",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["2", "4", "2", "1", "1", "4", "3", "2", "4", "1"]
-  },
-  {
-    name: "emily",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["2", "1", "5", "5", "4", "4", "2", "4", "3", "1"]
-  },
-  {
-    name: "emily",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["2", "1", "5", "5", "4", "4", "2", "4", "3", "1"]
-  },
-  {
-    name: "Emily",
-    photo:
-      "https://www.google.com/search?q=chinese+water+deer&rlz=1C1NDCM_enUS826US826&sxsrf=ACYBGNRfsAoD6IqhAlKt0kpR1HLZlw2qig:1570318936752&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiaw6LzpYblAhWQqp4KHZahA_cQ_AUIEigB&biw=1366&bih=657#",
-    scores: ["5", "4", "2", "1", "3", "2", "3", "2", "3", "3"]
-  },
-  {
-    name: "Rogelio R Zavala",
-    photo: "fs",
-    scores: ["3", "4", "3", "3", "2", "3", "3", "2", "4", "5"]
-  },
-  {
-    name: "Rogelio R Zavala",
-    photo: "fs",
-    scores: ["3", "4", "3", "3", "2", "3", "3", "2", "4", "5"]
-  },
-  {
-    name: "Rogelio R Zavala",
-    photo: "fs",
-    scores: ["3", "4", "3", "3", "2", "3", "3", "2", "4", "5"]
-  },
-  {
-    name: "Lola",
-    photo:
-      "https://c8.alamy.com/comp/MND3G3/vector-character-design-concept-superhero-woman-in-superwoman-pose-in-blue-and-red-costume-with-red-cape-and-purple-hair-MND3G3.jpg",
-    scores: ["3", "4", "3", "4", "3", "2", "4", "3", "3", "4"]
-  },
-  {
-    name: "Bobby Lightfoot",
-    photo:
-      "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/11/07/12/bigfoot-0.jpg?w968",
-    scores: ["4", "2", "4", "1", "1", "1", "1", "1", "1", "1"]
-  },
-  {
-    name: "Hi ",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
-  },
-  {
-    name: "me",
-    photo: "sdsddsvsv",
-    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-  },
-  {
-    name: "Lili",
-    photo:
-      "https://c8.alamy.com/comp/MND3G3/vector-character-design-concept-superhero-woman-in-superwoman-pose-in-blue-and-red-costume-with-red-cape-and-purple-hair-MND3G3.jpg",
-    scores: ["3", "1", "2", "1", "4", "2", "3", "2", "3", "2"]
-  },
-  {
-    name: "me",
-    photo: "sdsddsvsv",
-    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-  },
-  {
-    name: "giphyapp",
-    photo: "aa",
-    scores: ["2", "2", "3", "5", "3", "3", "4", "3", "2", "3"]
-  },
-  {
-    name: "giphyapp",
-    photo: "aa",
-    scores: ["2", "2", "3", "5", "3", "3", "4", "3", "2", "3"]
-  },
-  {
-    name: "giphyapp",
-    photo: "aa",
-    scores: ["2", "2", "3", "5", "3", "3", "4", "3", "2", "3"]
-  },
-  {
-    name: "giphyapp",
-    photo: "aa",
-    scores: ["2", "2", "3", "5", "3", "3", "4", "3", "2", "3"]
-  },
-  {
-    name: "Timmy",
-    photo:
-      "https://southparkstudios.mtvnimages.com/images/shows/south-park/episode-thumbnails/season-4/south-park-s04e04-timmy-2000_4x3.jpg",
-    scores: ["5", "2", "3", "5", "2", "3", "4", "2", "4", "1"]
-  },
-  {
-    name: "todo",
-    photo: "kkk",
-    scores: ["1", "2", "2", "1", "3", "3", "2", "2", "2", "1"]
-  },
-  {
-    name: "todo",
-    photo: "kkk",
-    scores: ["1", "2", "2", "1", "3", "3", "2", "2", "2", "1"]
-  },
-  {
-    name: "todo",
-    photo: "kkk",
-    scores: ["1", "2", "2", "1", "3", "3", "2", "2", "2", "1"]
-  },
-  {
-    name: "todo",
-    photo: "kkk",
-    scores: ["1", "2", "2", "1", "3", "3", "2", "2", "2", "1"]
-  },
-  {
-    name: "Patrick",
-    photo:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuRcYdvAf91LJVGqI_UANXErhA8qhUit4lSAZ451g0V5eP3Jrr",
-    scores: ["1", "3", "5", "1", "3", "5", "1", "1", "5", "3"]
-  },
-  {
-    name: "Peter",
-    photo: "adsdasd",
-    scores: ["3", "4", "4", "1", "4", "4", "3", "3", "5", "4"]
-  },
-  {
-    name: "a",
-    photo:
-      "https://www.google.com/imgres?imgurl=http%3A%2F%2Fclipart-library.com%2Fdata_images%2F233139.png&imgrefurl=http%3A%2F%2Fclipart-library.com%2Fgirl-cliparts.html&docid=UBf6GK1Jp0ChBM&tbnid=W5gpyOBubmz9UM%3A&vet=10ahUKEwiF4Luq_oDlAhUyIDQIHRH4C-EQMwh4KAAwAA..i&w=2854&h=5822&bih=722&biw=1088&q=clipart%20girl&ved=0ahUKEwiF4Luq_oDlAhUyIDQIHRH4C-EQMwh4KAAwAA&iact=mrc&uact=8",
-    scores: ["3", "2", "5", "2", "3", "4", "3", "3", "3", "3"]
-  },
-  {
-    name: "ss",
-    photo: "http://google.com",
-    scores: ["5", "4", "1", "1", "3", "5", "4", "2", "2", "2"]
-  },
-  {
-    name: "amjedayoub",
-    photo: "https://friend-finder-fsf.herokuapp.com/survey",
-    scores: ["3", "5", "3", "4", "3", "2", "4", "3", "3", "5"]
-  },
-  {
-    name: "Test",
-    photo: "https://images.app.goo.gl/p21doBUmkuWBhPC26",
-    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-  },
-  {
-    name: "Tesjhhjkh",
-    photo: "https://images.app.goo.gl/p21doBUmkuWBhPC26",
-    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-  },
-  {
-    name: "Tesjhhjkh",
-    photo: "https://images.app.goo.gl/p21doBUmkuWBhPC26",
-    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-  },
-  {
-    name: "Mandy",
-    photo: "https://www.istockphoto.com/photo/sunny-cat-gm508030340-85015717",
-    scores: ["2", "3", "2", "3", "3", "3", "3", "3", "3", "2"]
-  },
-  {
-    name: "Mandy",
-    photo: "https://www.istockphoto.com/photo/sunny-cat-gm508030340-85015717",
-    scores: ["2", "3", "2", "3", "3", "3", "3", "3", "3", "2"]
-  }
+  
 ];
